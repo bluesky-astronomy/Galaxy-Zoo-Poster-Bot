e1f8cbf4a07da5a5602143cec4c36471715496e8
AstroORyan <d.oryan@lancaster.ac.uk>
2024-04-27 09:54:32 +0100

    Updating the script of bot.

--- gz-bot.py
+++ gz-bot.py
@@ -2,7 +2,6 @@
 from atproto import Client
 
 import pandas as pd
-from PIL import Image
 import requests
 from io import BytesIO
 
@@ -14,27 +13,57 @@ def pull_galaxy_image(url):
     img_data = BytesIO(response.content)
     return img_data 
 
-def create_metadata(gal_info):
-    z = 
-    ra =
-    dec = 
-    clsf = 
-    instr =
-    survey =
-    projects =
-    metadata = (
-        ''
-    )
-
-def post(image, metadata, client):
-
-    response = client.send_image(text = metadata, image = image, image_alt = 'A Galaxy')
-
-    if len(response.errors) > 0:
-        print("error posting to BlueSky -- errors:")
-        print(response.errors)
+def create_metadata(row):
+    if type(row.redshift.iloc[0]) == str:
+        z = row.redshift.iloc[0]
     else:
-        print("successfully posted animation")
+        z = "%.2f" % ( row.redshift.iloc[0] )
+    ra = "%.5f" % row.ra.iloc[0]
+    dec = "%.5f" % row.dec.iloc[0]
+    clsf = row.galaxy_description.iloc[0]
+    survey = row.imaging.iloc[0]
+    if "CANDELS-COODS" in survey:
+        survey = 'CANDELS-GOODS'
+       
+    project = row.project.iloc[0]
+    if 'Hubble' in project:
+        instr = 'Hubble Space Telescope'
+    elif 'CANDELS' in project:
+        instr = 'Hubble Space Telescope'
+    elif 'Galaxy Zoo 2' in project:
+        instr = 'Apache Point 2.5m Telescope'
+
+    t_lookback = row.t_lookback.iloc[0]
+    if t_lookback < 1:
+        tmp = t_lookback * 1000
+        t_lookback_string = '%.3f million years' % tmp
+    else:
+        t_lookback_string = '%.2f billion years' % t_lookback
+    
+
+    metadata = (
+"""A {}, observed with the {} in the {} survey.
+
+It is at redshift {} (lookback time {}) with coordinates ({}, {}).
+
+This classification was made in the {} project.
+""").format(
+            clsf, instr, survey, z, t_lookback_string, ra, dec, project
+        )
+
+    return metadata
+
+def post(image, metadata, client, clsf, project):
+
+    alt_im_text = 'A {} from the {} project.' % (clsf, project)
+
+    response = (
+        client.send_image(
+            text = metadata, 
+            image = image, 
+            image_alt = alt_im_text
+        )
+    )
     
     return response
 
@@ -51,7 +80,7 @@ def main():
 
     # Selecting a Galaxy to upload.
     gal_data = pd.read_csv(cat_path).sample(1)
-    url = gal_data['url']
+    url = gal_data['image_url']
     gal_info = gal_data['some_meta_data']
 
     # Creating the Post
@@ -59,7 +88,7 @@ def main():
     post_string = create_metadata(gal_info)
 
     # Posting
-    response = post(image, post_string, client)
+    response = post(image, post_string, client, gal_data.galaxy_description.iloc[0], gal_data.project.iloc[0])
 
 ## Initialisation
 if __name__ == '__main__':
